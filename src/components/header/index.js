878439855e2a39ac29b85b7a9e4bad5204c70b40
DiegoValdera <47724955+DiegoValdera@users.noreply.github.com>
2021-12-04 12:52:10 -0500

    modificando header y agregando categoria

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -26,14 +26,7 @@ const Nav = () => {
 
     return (
     <ul className="header__ul">
-        <li className="header__li"><Link exact to="/usados" activeClassName="a_active">Usados</Link></li>
-        <li className="header__li"><Link exact to="2" activeClassName="a_active">Financiación</Link></li>
-        <li className="header__li"><Link exact to="/buscar-auto" activeClassName="a_active">Catalogo</Link>
-            <ul>
-                <li><Link exact to="/buscar-autos" activeClassName="a_active">Autos</Link></li>
-                <li><Link exact to="/buscar-autos" activeClassName="a_active">Motos</Link></li>
-            </ul>
-        </li>
+        <li className="header__li"><Link exact to="/buscar-auto?search=0&brand=0&model=0&year_from=0&year_to=0&price=0" activeClassName="a_active">Catalogo</Link></li>
         <li className="header__li"><Link exact to="/vende" activeClassName="a_active">Vende tu auto</Link></li>
         {!isLogin ? (<>
           <li className="header__li"><Link exact to="/register" activeClassName="a_active">Registrate</Link></li>
