d38a0c2e32810fecf4c76d18f3d751894c7abc9e
Jordan <jordan.terrazos@gmail.com>
2021-12-05 09:10:14 -0500

    update

--- src/pages/autos-publicados/index.jsx
+++ src/pages/autos-publicados/index.jsx
@@ -1,15 +1,38 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import CardCarPublish from './card-car-publicado';
 import imgprueba1from from '../../img/auto.png';
 import { NavLink as Link } from 'react-router-dom';
+import { api } from "../../api/api";
+import { useEffect, useState } from 'react';
+import swal from 'sweetalert';
+import { useHistory } from 'react-router-dom';
 
 const CarsPublished = () => {
+
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const dataCarsPublished = useSelector((state) => {
     return state.publsh.cars_published;
   });
 
   console.log("dataCarsPublished",dataCarsPublished);
+
+  const handleRemoveBox = async(id) =>{
+    console.log(id);
+    await api.delete(`/ofertas/${id.target.value}`);
+  }
+
+  const [carsDataPublished, getCarsDataPublished] = useState([]);
+
+  const getCarsPublished = async () => {
+    const CarsPublished = await api.get("/ofertas");
+    return CarsPublished.data;
+  }
+
+  const userData = useSelector((state) => {
+    return state.auth.userData;
+  });
 
   return(
     <div className="main_buscar_auto_publish">
@@ -18,44 +41,47 @@ const CarsPublished = () => {
         {Object.keys(dataCarsPublished).length == 0 ? '' : dataCarsPublished.map((e)=>{
           return(
           <div className="publishCar__data">
-          <h1>Publicación</h1>
-          <div>
+            <h1>Publicación</h1>
             <div>
-              <img src={e.photos[0]}/>
-            </div>
-            <div className="publishCar__dataInput">
               <div>
-                <h2>Marca</h2>
-                <h2>{e.marca}</h2>
+                <img src={e.photos[0]}/>
               </div>
-              <div>
-                <h2>Transmisión</h2>
-                <h2>{e.transmision}</h2>
-              </div>
-              <div>
-                <h2>Kilometraje</h2>
-                <h2>{e.kilometraje}</h2>
-              </div>
-              <div>
-                <h2>Modelo</h2>
-                <h2>{e.modelo}</h2>
-              </div>
-              <div>
-                <h2>Año de frabricación</h2>
-                <h2>{e.añomfabricacion}</h2>
-              </div>
-              <div>
-                <h2>Combustible</h2>
-                <h2>{e.combustible}</h2>
-              </div>
+              <div className="publishCar__dataInput">
+                <div>
+                  <h2>Marca</h2>
+                  <h2>{e.marca}</h2>
+                </div>
+                <div>
+                  <h2>Transmisión</h2>
+                  <h2>{e.transmision}</h2>
+                </div>
+                <div>
+                  <h2>Kilometraje</h2>
+                  <h2>{e.kilometraje}</h2>
+                </div>
+                <div>
+                  <h2>Modelo</h2>
+                  <h2>{e.modelo}</h2>
+                </div>
+                <div>
+                  <h2>Año de frabricación</h2>
+                  <h2>{e.añomfabricacion}</h2>
+                </div>
+                <div>
+                  <h2>Combustible</h2>
+                  <h2>{e.combustible}</h2>
+                </div>
             </div>
           </div>
+          {/* <div  className="butonDeleteCars">
+            <button value={e.id} onClick={handleRemoveBox} >X</button>
+          </div> */}
         </div>)
         })
 
       }
       </div>
-      <div>
+      <div className="btnPublicarCars">
         <Link to="/publicar-auto">
           <button>Publicar</button>
         </Link>
